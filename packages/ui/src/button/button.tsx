a864b2a227a27c332f3fae2efe9c7c07bbea2bd9
josellia <joseliaapires@hotmail.com>
2024-03-16 13:13:54 -0300

    feat: props button ui

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -8,16 +8,21 @@ export interface Props {
 	className: string;
 	type: 'button' | 'submit' | 'reset';
 	label: string;
+	isLoading: boolean;
+	isDisabled: boolean;
+	variant: 'primary' | 'secondary' | 'danger' | 'success' | 'warning' | 'info' | 'light' | 'dark';
 }
 
 export interface ButtonProps extends Partial<Props> {}
 
-export const Button = ({ label, onclick, className, type, children }: ButtonProps) => {
+export const Button = ({ label, onclick, className, type, children, isDisabled }: ButtonProps) => {
+	
 	return (
 		<button
-			className={`${styles.basic} ${className || 'primary'}`}
+			className={`${styles.basic} ${className}`}
 			aria-label={label}
 			type={type}
+			disabled={isDisabled}
 			onClick={onclick}
 			>
             {children}
